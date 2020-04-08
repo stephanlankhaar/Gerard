6c97cea9f37f85c8e1c309b4eac02893b00c1615
stephanlankhaar <stephanlankhaar@yahoo.com>
2020-04-08 16:47:19 +0200

    more testing for homescreen en shit

--- src/homescreen.cpp
+++ src/homescreen.cpp
@@ -1,7 +1,6 @@
 #include<ncurses.h>
 #include<iostream>
 #include<sqlite3.h>
-#include<string>
 
 using namespace std;
 
@@ -9,49 +8,32 @@ int main(){
   initscr();
   cbreak();
 
-  WINDOW * homescreen = newwin(20,50,0,0);
-  box(homescreen,0,0);
+  char const * tasks[]={"Task 1", "Task 2", "Task 3"};
+  WINDOW*menu=newwin(10,100,0,0);
+  box(menu,0,0);
   refresh();
-  wrefresh(homescreen);
-  keypad(homescreen,true);
+  wrefresh(menu);
+  keypad(menu,true);
 
-  string choices[]={"task 1", "task 2", "task 3", "task 4"};
-  int choice;
+  int choise;
   int highlight;
-
-  while(1){
-    for(int i=0; i<3; i++){
-      if(i==highlight){
-        wattron(homescreen,A_REVERSE);
-        mvprintw(i+1,1,choices[i].c_str());
-        wattroff(homescreen,A_REVERSE);
-      }
-    }
-    choice=wgetch(homescreen);
-    switch(choice){
-      case KEY_UP:
-        highlight--;
-        if(highlight ==-1){
-          highlight=0;
-        } 
-        break;
-      case KEY_DOWN:
-        highlight++;
-        if(highlight == 4){
-          highlight =2;
-        }
-        break;
-      default:
-        break;
-    }
-    if(choice == 10){
-      break;
-    }
+  int y=1;
+  for(int i=0; i < 3; i++){
+    mvwprintw(menu, y,1,tasks[i]);
+    y++;
   }
-  clear();
 
-  printw("You've selected %s", choices[highlight].c_str());
+  int userInput=wgetch(menu);
+  switch(userInput){
+    case KEY_F(9):
+      clear();
+      mvprintw(0,0,"You've opened the menu Yay!");
+      break;
+    default:
+      break;
+  }
 
+  wrefresh(menu);
   getch();
   endwin();
 }
