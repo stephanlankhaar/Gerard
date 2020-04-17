e1af565f71346e50d59eaaa1594378aa89eaddeb
stephanlankhaar <stephanlankhaar@yahoo.com>
2020-04-17 12:40:16 +0200

    New task fixed in db

--- src/database.cpp
+++ src/database.cpp
@@ -7,16 +7,61 @@
 
 using namespace std;
 
-static int callback(void *data, int argc, char **argv, char **azColName){
+static int idCallback(void *data, int argc, char**argv, char **azColName){
+  int i;
+  fprintf(stderr,"%s:", (const char*)data);
+
+  for(i=0;i<argc;i++){
+    printf("%i\r\n",atoi(argv[i]));
+  }
+  int taskId=atoi(argv[0]);
+  printf("%i\r\n",taskId);
+  return 0;
+}
+
+static int stdCallback(void *data, int argc, char **argv, char **azColName){
   int i;
   fprintf(stderr, "%s: ", (const char*)data);
    
   for(i = 0; i<argc; i++){
      printf("%s = %s\r\n", azColName[i], argv[i] ? argv[i] : "NULL");
   }
-   
+  int taskId=atoi(argv[0]);
   printf("\r\n");
   return 0;
+}
+int getTaskId(){
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  int rc;
+  const char *sql;
+  const char* data = "stdCallback function called";
+  int taskId=0;
+
+  /* Open database */
+  rc = sqlite3_open("tasks.db", &db);
+  
+  if( rc ) {
+     fprintf(stderr, "Can't open database: %s\r\n", sqlite3_errmsg(db));
+     return(0);
+  } else {
+     fprintf(stderr, "Opened database successfully\r\n");
+  }
+
+  /* Create SQL statement */
+  sql = "SELECT MAX(taskId) FROM tasks";
+
+  /* Execute SQL statement */
+  rc = sqlite3_exec(db, sql, idCallback, (void*)data, &zErrMsg);
+  
+  if( rc != SQLITE_OK ) {
+     fprintf(stderr, "SQL error: %s\r\n", zErrMsg);
+     sqlite3_free(zErrMsg);
+  } else {
+     fprintf(stdout, "Operation done successfully\r\n");
+  }
+  sqlite3_close(db);
+  return taskId;
 }
 
 int selectFromDb(){
@@ -24,7 +69,7 @@ int selectFromDb(){
   char *zErrMsg = 0;
   int rc;
   const char *sql;
-  const char* data = "Callback function called";
+  const char* data = "stdCallback function called";
 
   /* Open database */
   rc = sqlite3_open("tasks.db", &db);
@@ -40,7 +85,7 @@ int selectFromDb(){
   sql = "SELECT * from TASKS";
 
   /* Execute SQL statement */
-  rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+  rc = sqlite3_exec(db, sql, stdCallback, (void*)data, &zErrMsg);
   
   if( rc != SQLITE_OK ) {
      fprintf(stderr, "SQL error: %s\r\n", zErrMsg);
@@ -59,6 +104,7 @@ string getUserInput(string question){
   return answer;
 }
 
+
 int writeToDb(){
   string newTask = getUserInput("Type your new task here: ");
   sqlite3 *db;
@@ -76,15 +122,16 @@ int writeToDb(){
   }
 
   /* Create SQL statement */
-  int taskId=1;
+  int taskId=6;
   char sql[500];
-  snprintf(sql,500 ,"INSERT INTO tasks(taskId,TASK) " \
+  snprintf(sql,500, "INSERT INTO tasks(`task`)"\
+                    "VALUES('%s');",(newTask).c_str());
+  //snprintf(sql,500 ,"INSERT INTO tasks(taskId,TASK) " \
         "VALUES (%d, '%s');",taskId, (newTask).c_str());
   //sql=strcat(strcat("INSERT INTO tasks(taskId,TASK) VALUES (1, '", newTask), "');");
   //sql = "INSERT INTO tasks(taskId,TASK) "  \
         "VALUES (1, '" + newTask + "'); ";
 
-        cout << "test";
 /*
  * INSERT INTO TASKS(ID, TASK) 
  * VALUES(1, "Test");
@@ -93,7 +140,7 @@ int writeToDb(){
  *
  */
   /* Execute SQL statement */
-  rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+  rc = sqlite3_exec(db, sql, stdCallback, 0, &zErrMsg);
   
   if( rc != SQLITE_OK ){
      fprintf(stderr, "SQL error: %s\r\n", zErrMsg);
@@ -101,7 +148,6 @@ int writeToDb(){
   } else {
      fprintf(stdout, "Records created successfully\r\n");
   }
-  sqlite3_close(db);
   return 0;
 }
 
@@ -109,8 +155,8 @@ int main(int argc, char* argv[]) {
   initscr();
 
   writeToDb();
+  getch();
   selectFromDb();
-
   getch();
   endwin();
   return 0;
